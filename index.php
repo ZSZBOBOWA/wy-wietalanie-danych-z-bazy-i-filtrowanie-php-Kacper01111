4be286eecfed05fecaa0a162c5277af1c6d80aa5
KacperMatusik <Kacper.Matusik@zsz.bobowa.pl>
2024-10-24 08:38:24 +0200

    sa

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form method="POST" action="index.php">
+    Wpisz nazwisko: <input type="text" name="nazwisko">
+    <input type="submit" value="Filtruj">
+</form>
+
 <?php
 $servername = "localhost";
 $username = "root";
@@ -6,8 +20,13 @@ $dbname = "szkola3";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn){die("Błąd Połączenia: " . mysqli_connect_error());
 }
+if(isset($_POST['nazwisko']) && $_POST['nazwisko'] != '') {
+    $nazwisko = $_POST['nazwisko'];
+    $nazwisko = mysqli_real_escape_string($conn, $nazwisko);
+    $sql = "SELECT * FROM uczniowie WHERE nazwisko='$nazwisko'";
+}else {$sql = "SELECT * FROM uczniowie";
+}
 
-$sql = "SELECT * FROM uczniowie";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     echo "<table border='1'<tr><th>Imię</th><th>Nazwsiko</th><th>Wiek</th></tr>";
@@ -19,3 +38,5 @@ if (mysqli_num_rows($result) > 0) {
 }
 mysqli_close($conn);
 ?>
+</body>
+</html>
