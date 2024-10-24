8f15737c77a2ce217b7c465b7d07561e4c88e95b
KacperMatusik <Kacper.Matusik@zsz.bobowa.pl>
2024-10-24 08:29:20 +0200

    s

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "szkola";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn){die("Błąd Połączenia: " . mysqli_connect_error());
+}
+?>
