706594a30841705bb27979b772b1b02042c5ce2f
KacperMatusik <Kacper.Matusik@zsz.bobowa.pl>
2024-10-24 08:32:31 +0200

    2

--- index.php
+++ index.php
@@ -6,4 +6,16 @@ $dbname = "szkola";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn){die("Błąd Połączenia: " . mysqli_connect_error());
 }
+
+$sql = "SELECT * FROM uczniowie";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    echo "<table border='1'<tr><th>Imię</th><th>Nazwsiko</th><th>Wiek</th></tr>";
+    while ($row = mysqli_fetch_assoc($result)){
+        echo "<tr><td>".$row["imie"]."</td><td>".$row["nazwisko"]."</td><td>".$row["wiek"]."</td></tr>";
+    }
+    echo "</table>";
+} else {echo "Brak wyników";
+}
+mysqli_close($conn);
 ?>
